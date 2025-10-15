8edd7f80ddd7a405eef4c9df7ef8491c7a7ee5ae
Nijam-Uddin-Qudraty <129588187+Nijam-Uddin-Qudraty@users.noreply.github.com>
2025-10-15 16:35:21 +0600

    add filter

--- usermanagement/dashboard/serializers.py
+++ usermanagement/dashboard/serializers.py
@@ -4,5 +4,4 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'is_active', 'is_staff']
-        read_only_fields = ['id']
+        fields = "__all__"
--- usermanagement/dashboard/views.py
+++ usermanagement/dashboard/views.py
@@ -10,7 +10,7 @@ from rest_framework import viewsets, pagination
 from rest_framework.permissions import IsAdminUser
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class UserPagination(pagination.PageNumberPagination):
     page_size = 100
@@ -23,3 +23,5 @@ class DashboardView(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [IsAdminUser]
     pagination_class = UserPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['username', 'email', 'is_staff', 'is_active']
